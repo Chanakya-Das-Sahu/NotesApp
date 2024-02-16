72e4bb841fc4e2d168d5a25ff0a594082d27f247
ChandaniSahu <140198472+ChandaniSahu@users.noreply.github.com>
2024-02-16 22:41:24 +0530

    after trial successfull

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,61 +1,13 @@
-// const express = require('express');
-// const userSchema = require('../models/User');
-// const router = express.Router();
-// const argon2 = require('argon2');
-// const jwt = require('jsonwebtoken');
-// const cookieParser = require('cookie-parser')
-
-// router.post('/signup', async (req, res) => {
-//     const { email, password } = req.body;
-//     const hashedPass = await argon2.hash(password)
-//     const check = await userSchema.findOne({ email: email})
-    
-//     if (check) {
-//      res.json({msg:'found'})
-//     } else {
-
-//         const newUser = await new userSchema({
-//             email, password:hashedPass
-//         });
-
-//         try {
-//             await newUser.save();
-//             res.json({ msg:'created'})
-//         } catch (err) {
-//             console.log(`error due to ${err}`);
-//         }
-//     }
-// });
-
-// router.post('/login', async (req, res) => {
-//     const { email, password } = req.body;
-//     const user = await userSchema.findOne({email});
-//     let check , token;
-//     if(user){
-//     check = await argon2.verify(user.password,password)
-//     token = jwt.sign({email:user.email,id:user._id},'your_secret_key',{expiresIn:'5s'})
-//     }
-//     // console.log(token)
-//     if(check){
-//         res.json({msg:'found',token})
-//         // console.log({msg:'found',user})
-//     }else{
-//         res.json({msg:'not found'})
-//     }
-// });
-
-
-// module.exports = router;
-
-
 const express = require('express');
 const userSchema = require('../models/User');
 const router = express.Router();
+const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
+    const hashedPass = await argon2.hash(password)
     const check = await userSchema.findOne({ email: email})
     
     if (check) {
@@ -63,7 +15,7 @@ router.post('/signup', async (req, res) => {
     } else {
 
         const newUser = await new userSchema({
-            email, password
+            email, password:hashedPass
         });
 
         try {
@@ -77,11 +29,16 @@ router.post('/signup', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    const user = await userSchema.findOne({email,password}).select('-password');
-    let token;
+    const user = await userSchema.findOne({email});
+    let check , token;
     if(user){
+    check = await argon2.verify(user.password,password)
     token = jwt.sign({email:user.email,id:user._id},'your_secret_key',{expiresIn:'5s'})
+    }
+    // console.log(token)
+    if(check){
         res.json({msg:'found',token})
+        // console.log({msg:'found',user})
     }else{
         res.json({msg:'not found'})
     }
@@ -89,3 +46,46 @@ router.post('/login', async (req, res) => {
 
 
 module.exports = router;
+
+
+// const express = require('express');
+// const userSchema = require('../models/User');
+// const router = express.Router();
+// const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser')
+
+// router.post('/signup', async (req, res) => {
+//     const { email, password } = req.body;
+//     const check = await userSchema.findOne({ email: email})
+    
+//     if (check) {
+//      res.json({msg:'found'})
+//     } else {
+
+//         const newUser = await new userSchema({
+//             email, password
+//         });
+
+//         try {
+//             await newUser.save();
+//             res.json({ msg:'created'})
+//         } catch (err) {
+//             console.log(`error due to ${err}`);
+//         }
+//     }
+// });
+
+// router.post('/login', async (req, res) => {
+//     const { email, password } = req.body;
+//     const user = await userSchema.findOne({email,password}).select('-password');
+//     let token;
+//     if(user){
+//     token = jwt.sign({email:user.email,id:user._id},'your_secret_key',{expiresIn:'5s'})
+//         res.json({msg:'found',token})
+//     }else{
+//         res.json({msg:'not found'})
+//     }
+// });
+
+
+// module.exports = router;
