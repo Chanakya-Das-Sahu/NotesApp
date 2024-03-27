44c0f2f03ec99a6ee60be2d97727b1801b548b15
Chanakya Das Sahu <109864238+Chanakya-Das-Sahu@users.noreply.github.com>
2024-03-27 19:03:40 +0530

    expired.jsx : URL corrected to render

--- frontend/src/expired.jsx
+++ frontend/src/expired.jsx
@@ -21,7 +21,7 @@ const Expired = ({ setLogin }) => {
     const handleLogin = async () => {
         try {
 
-            const res = await axios.post('http://localhost:3000/user/login', data);
+            const res = await axios.post('https://notesapp-roks.onrender.com/user/login', data);
 
             if (res.data.msg == 'found') {
              
