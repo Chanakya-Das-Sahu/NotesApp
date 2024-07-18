b81f75a46c0a64fe87a473d4a19f5b141a068a79
Chanakya Das Sahu <ccccsahu@gmail.com>
2024-07-18 14:40:22 +0530

    syntax error

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-// const mongoURI = process.env.MONGO_URI ;
+// require('dotenv').config();
+const mongoURI = process.env.MONGO_URI ;
 const connectToMongo = () =>{
     mongoose.connect(mongoURI).then(()=>{console.log('connected to mongoDB.')}).catch();
 }
